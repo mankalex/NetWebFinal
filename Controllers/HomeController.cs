a1f4a077ba71edb9958cd8209f860bccc45a4dd5
Mona Lisa <email@example.com>
2023-11-30 19:05:39 -0600

    [1.03]

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,8 @@ namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() => View();
+        private DataContext _dataContext;
+        public HomeController(DataContext db) => _dataContext = db;
+        public ActionResult Index() => View(_dataContext.Discounts.Where(d => d.StartTime <= DateTime.Now && d.EndTime > DateTime.Now).Take(3));
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+// Connection info stored in appsettings.json
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(configuration["Data:Northwind:ConnectionString"]));
 var app = builder.Build();
 // Test
 
